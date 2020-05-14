c9eeb35fbf4a887fcb176909968d659795917f42
Crowdbotics <team@crowdbotics.com>
2020-05-14 09:21:33 +0000

    Update models

--- backend/home/migrations/0007_auto_20200514_0736.py
+++ backend/home/migrations/0007_auto_20200514_0736.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0006_auto_20200514_0734'),
+        ("home", "0006_auto_20200514_0734"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='customtext',
-            name='bbbgv',
+            model_name="customtext",
+            name="bbbgv",
             field=models.BigIntegerField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='customtext',
-            name='bgvhjv',
+            model_name="customtext",
+            name="bgvhjv",
             field=models.BigIntegerField(blank=True, null=True),
         ),
     ]
